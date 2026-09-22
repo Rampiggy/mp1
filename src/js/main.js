134a1d4864849261cbd02f1aa0b154f3f36d08cc
Rampiggy <git@rampiggy.dev>
2026-09-22 09:25:25 -0500

    Make navbar resize once it hits the top of the screen

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,23 @@ picOfMe.addEventListener("click", (e) => {
 	picOfMeDialog.showModal();
 });
 
+const navListItems = document.querySelectorAll("nav ul li");
 window.addEventListener("scroll", () => {
+	const navScrollY = document.querySelector("nav").getBoundingClientRect().y;
+	if (navScrollY === 0) {
+		navListItems.forEach((navListItem) => {
+			navListItem.style.fontSize = "34px";
+			navListItem.style.margin = "3px";
+		});
+	} else {
+		navListItems.forEach((navListItem) => {
+			navListItem.style.fontSize = "40px";
+			navListItem.style.margin = "10 0px";
+		});
+	}
+
+	const currScrollY = window.scrollY;
 	const projectsScrollY = document
 		.querySelector("#projects")
 		.getBoundingClientRect().y;
-	const currScrollY = window.scrollY;
-
-	if (currScrollY > projectsScrollY) {
-	}
 });
