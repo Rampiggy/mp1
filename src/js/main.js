3e45de6b397b135cd92e8ad719471ae62ef90e06
Rampiggy <git@rampiggy.dev>
2026-09-22 08:31:22 -0500

    Fix padding on unordered lists and make navbar width of whole page

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,16 @@
 const picOfMe = document.querySelector(".top img");
 const picOfMeDialog = document.querySelector(".top dialog");
+
 picOfMe.addEventListener("click", (e) => {
 	picOfMeDialog.showModal();
 });
 
-/*
-const projectsHeader = document.querySelector("#projects");
 window.addEventListener("scroll", () => {
-	const currScroll = window.scrollY;
-	const projectsY = projectsHeader.getBoundingClientRect().y;
-	if ()
+	const projectsScrollY = document
+		.querySelector("#projects")
+		.getBoundingClientRect().y;
+	const currScrollY = window.scrollY;
+
+	if (currScrollY > projectsScrollY) {
+	}
 });
-*/
