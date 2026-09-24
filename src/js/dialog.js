9cc6dca4482752a60d002f6ba3d1204daf35da3a
Rampiggy <git@rampiggy.dev>
2026-09-24 13:50:50 -0500

    Add close dialog svg

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -1,6 +1,11 @@
-const picOfMe = document.querySelector(".top img");
 const picOfMeDialog = document.querySelector(".top dialog");
 
+const picOfMe = document.querySelector("#profile-pic");
 picOfMe.addEventListener("click", (e) => {
 	picOfMeDialog.showModal();
+});
+
+const closeDialog = document.querySelector("#close-dialog");
+closeDialog.addEventListener("click", (e) => {
+	picOfMeDialog.close();
 });
